edc92043fb9e58741763bb50c60df70a05784491
Philipp Dallmann <philipp.dallmann@outlook.com>
2023-10-19 21:08:59 +0200

    feat: initial vite react setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button>
+          Button
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
